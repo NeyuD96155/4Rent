502126d96ba2ad96e8c832a69d9b97165a652cd2
SongThang <thangnstse171771@fpt.edu.vn>
2024-01-30 19:38:00 +0700

    import HomePageContent vào HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -7,27 +8,25 @@ import AboutUs from "../components/AboutUs";
 import Search from "../components/Search";
 import SignUp from "../components/SignUp";
 import SignIn from "../components/SignIn";
-import { GettingStarted, ExchangeTimeshares, MaximizeVacation } from "../components/GuidesCom/GettingStarted";
+import HomePageContent from "../components/HomePageContent";  // Import HomePageContent
+
 function Homepage() {
-    return (
-        <Router>
-            <NavigationBar />
-            <Routes>
-                <Route path="/"/>
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/guides" element={<Guides />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/about" element={<AboutUs />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/getting-started" element={<GettingStarted />} />
-                    <Route path="/exchange-timeshares" element={<ExchangeTimeshares />} />
-                    <Route path="/maximize-vacation" element={<MaximizeVacation />} />
-                {/* Define other routes here */}
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePageContent />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/guides" element={<Guides />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/search" element={<Search />} />
+        {/* Các Route khác */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default Homepage;
