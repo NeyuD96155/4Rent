ae2a9599c1e5cfcd4abe20a631dde413e54616f9
SongThang <thangnstse171771@fpt.edu.vn>
2024-01-31 19:59:41 +0700

    them DetailPage vào HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,36 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavigationBar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Guides from "../components/Guides";
-import Contact from "../components/Contact";
-import AboutUs from "../components/AboutUs";
-import Search from "../components/Search";
-import SignUp from "../components/SignUp";
-import SignIn from "../components/SignIn";
-import { GettingStarted, ExchangeTimeshares, MaximizeVacation } from "../components/GuidesCom/GettingStarted";
-import HomePageContent from "../components/HomePageContent";  // Import HomePageContent
+// Homepage.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Guides from '../components/Guides';
+import Contact from '../components/Contact';
+import AboutUs from '../components/AboutUs';
+import Search from '../components/Search';
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
+import { GettingStarted, ExchangeTimeshares, MaximizeVacation } from '../components/GuidesCom/GettingStarted';
+import HomePageContent from '../pages/HomePageContent';  // Import HomePageContent
+import DetailPage from '../pages/DetailPage';
 
 function Homepage() {
-    return (
-        <Router>
-            <NavigationBar />
-            <Routes>
-                <Route path="/" element={<HomePageContent />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/guides" element={<Guides />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/about" element={<AboutUs />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/getting-started" element={<GettingStarted />} />
-                <Route path="/exchange-timeshares" element={<ExchangeTimeshares />} />
-                <Route path="/maximize-vacation" element={<MaximizeVacation />} />
-                {/* Define other routes here */}
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePageContent />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/guides" element={<Guides />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/getting-started" element={<GettingStarted />} />
+        <Route path="/exchange-timeshares" element={<ExchangeTimeshares />} />
+        <Route path="/maximize-vacation" element={<MaximizeVacation />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        {/* Define other routes here */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default Homepage;
