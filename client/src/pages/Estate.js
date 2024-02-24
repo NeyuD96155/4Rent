5a6750bb5d2e998086714c2fc14b140c48f9cd8f
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-02-24 20:46:21 +0700

    edit card item bằng nhau

---
+++ client/src/pages/Estate.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import api from "../config/axios";
+import { format } from 'date-fns';
+import { useNavigate } from 'react-router-dom'; 
+import '../styles/Estate.css';
+
+const TimesharePosts = () => {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/post/show');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching timeshare posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handlePostClick = (postId) => {
+    navigate(`/post/detail/${postId}`); 
+  };
+
+  return (
+    <div className="timeshare-posts-container">
+      <h1>Timeshare Posts</h1>
+      <div className="posts">
+        {posts.map((post, index) => (
+          <div key={index} className="post-card" onClick={() => handlePostClick(post.id)}>
+            <img 
+              className="post-image"
+              src={post.resources.length > 0 ? post.resources[0].url : 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg'}
+              alt="Post"
+            />
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <p>Price: ${post.price}</p>
+            <p>Date Posted: {format(new Date(post.postDate), 'PPP')}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TimesharePosts;
