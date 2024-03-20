ce22b44f5521a516eb9b40e90b75aa33a20ba322
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-03-20 22:27:40 +0700

    merge 2 repo

--- models/Payment.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const paymentSchema = new mongoose.Schema({
-    // Define fields as per requirement
-});
-
-module.exports = mongoose.model('Payment', paymentSchema);
