27cc64304625dddbcb6d68a81059e2f339247dcf
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-03-27 16:45:07 +0700

    deploy

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,5 +1,5 @@
 echo "Building app..."
 npm run build
 echo "Deploy files to server..."
-scp -r  build/* root@128.199.123.86:/var/www/html/
+scp -r  build/* root@128.199.126.249:/var/www/html/
 echo "Done!"
